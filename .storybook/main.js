5fa7a8374dd7d82651fa900952306c0fc2fe47f1
huangjien <huangjien@gmail.com>
2023-02-01 22:48:12 +0000

    story book works

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+export default {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag"
+  }
+};
