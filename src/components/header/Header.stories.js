f3e9ec30a738020a07df44b3bb8abde5244decb3
huangjien <huangjien@gmail.com>
2023-02-06 20:23:12 +0000

    try redeploy

--- src/components/header/Header.stories.js
+++ src/components/header/Header.stories.js
@@ -1,4 +1,4 @@
-import { Header } from './Header';
+import Header from './Header';
 import { mockHeaderProps } from './Header.mock';
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,4 @@
-
-import { Inter } from '@next/font/google';
 import Layout from '../components/layout/Layout';
-
-const inter = Inter({ subsets: ['latin'] });
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Layout>
-//         <p>Hello, My blog</p>
-//       </Layout>
-//     </>
-//   );
-// }
 
 const Index = () => (
   <Layout>
