75f118dfb9a6166af7e1c7ab8ca3a56e1b81e7c5
huangjien <huangjien@gmail.com>
2023-04-13 01:43:30 +0100

    docker works but too big

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { darkTheme, globalStyle, lightTheme } from '../lib/themes';
 import { ProvideAuth } from '../lib/useAuth';
 import { ProvideSettings } from '../lib/useSettings';
 import '../locales/i18n';
 import '../styles/github-markdown.css';
-import { darkTheme, globalStyle, lightTheme } from './themes';
 
 export default function App({ Component, pageProps }) {
   globalStyle();
