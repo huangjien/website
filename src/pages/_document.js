b4df3e1873d89cf774e7a3521ef79bcddee7edac
huangjien <huangjien@gmail.com>
2023-10-09 13:47:33 +0100

    pwa ready

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,6 +15,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon-512x512.png' />
+        <meta name='theme-color' content='#000' />
         <meta name="emotion-insertion-point" content="" />
       </Head>
       <body>
