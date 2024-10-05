eabbf9ab9f5c5d43d7e2eb9a8db2515ee7152059
huangjien <huangjien@gmail.com>
2024-10-05 22:22:25 +0100

    add google login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@
 
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +10,10 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here if needed
   ],
