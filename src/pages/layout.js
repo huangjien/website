ec9ac3f72bca05c536cf47b612b02c2cecf64eb6
huangjien <huangjien@gmail.com>
2024-10-06 11:07:00 +0100

    add tailwind indicator

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -53,6 +53,13 @@ export default function RootLayout({ children }) {
           </Link>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{' version: ' + packageJson.version}
         </p>
+        <div class="fixed right-0 z-50 bg-pink-500 text-white shadow-md px-2 rounded-bl ">
+          <span class="sm:hidden">default</span>
+          <span class="hidden sm:inline md:hidden">sm</span>
+          <span class="hidden md:inline lg:hidden">md</span>
+          <span class="hidden lg:inline xl:hidden">lg</span>
+          <span class="hidden xl:inline">xl</span>
+        </div>
       </footer>
     </main>
 
