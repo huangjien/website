603fb588ef25f441a4146964b87ebcb111847b98
huangjien <huangjien@gmail.com>
2026-04-12 18:38:16 +0100

    entrypoint.sh

--- deploy/nginx/entrypoint.sh
+++ deploy/nginx/entrypoint.sh
@@ -15,16 +15,4 @@ envsubst '${RELAY_UPSTREAM_HOST} ${RELAY_UPSTREAM_PORT}' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
-attempt=0
-max_attempts="${RELAY_READY_MAX_ATTEMPTS:-30}"
-sleep_seconds="${RELAY_READY_SLEEP_SECONDS:-1}"
-while ! nc -z "$RELAY_UPSTREAM_HOST" "$RELAY_UPSTREAM_PORT" >/dev/null 2>&1; do
-  attempt=$((attempt + 1))
-  if [ "$attempt" -ge "$max_attempts" ]; then
-    echo "relay readiness failed for ${RELAY_UPSTREAM_HOST}:${RELAY_UPSTREAM_PORT}" >&2
-    exit 1
-  fi
-  sleep "$sleep_seconds"
-done
-
 exec nginx -g 'daemon off;'
